8e3844cd0606d94b9e074fdb689856d2de693b73
basiliohadnan@gmail.com <renato.gelder@ituran.com.br>
2024-08-23 17:27:08 -0300

    project renaming

--- SAPTests/run tests - done.bat
+++ SAPTests/run tests - done.bat
@@ -6,10 +6,10 @@ set /p executionType=Defina a exec. desejada (1 ou 2):
 
 if "%executionType%"=="1" (
     echo Running all tests...
-    dotnet test --filter "TestCategory=done" "C:\source\repos\assai\SAPTests\SAPTests.csproj" -p:NoWarn=true -p:WarningLevel=0 --logger trx
+    dotnet test --filter "TestCategory=done" "C:\Users\basil\OneDrive\Documentos\GitHub\LIT\poc-sap-test\SAPTests\SAPTests.csproj" -p:NoWarn=true -p:WarningLevel=0 --logger trx
 ) else if "%executionType%"=="2" (
     echo Resuming execution...
-    dotnet test --filter "TestCategory=done" "C:\source\repos\assai\SAPTests\SAPTests.csproj" -p:NoWarn=true -p:WarningLevel=0 --logger trx
+    dotnet test --filter "TestCategory=done" "C:\Users\basil\OneDrive\Documentos\GitHub\LIT\poc-sap-test\SAPTests\SAPTests.csproj" -p:NoWarn=true -p:WarningLevel=0 --logger trx
 ) else (
     echo Invalid executionType. Exiting...
     exit /b 1
