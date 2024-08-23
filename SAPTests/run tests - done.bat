3e3f8a410b78084a01b13486eae1de8f06c58481
basiliohadnan@gmail.com <renato.gelder@ituran.com.br>
2024-08-23 17:22:21 -0300

    renamed project

---
+++ SAPTests/run tests - done.bat
@@ -0,0 +1,16 @@
+@echo off
+echo 1. Executar todos os testes.
+echo 2. Retomar exec.
+echo.
+set /p executionType=Defina a exec. desejada (1 ou 2):
+
+if "%executionType%"=="1" (
+    echo Running all tests...
+    dotnet test --filter "TestCategory=done" "C:\source\repos\assai\SAPTests\SAPTests.csproj" -p:NoWarn=true -p:WarningLevel=0 --logger trx
+) else if "%executionType%"=="2" (
+    echo Resuming execution...
+    dotnet test --filter "TestCategory=done" "C:\source\repos\assai\SAPTests\SAPTests.csproj" -p:NoWarn=true -p:WarningLevel=0 --logger trx
+) else (
+    echo Invalid executionType. Exiting...
+    exit /b 1
+)
